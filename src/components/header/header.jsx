a032554eb71d00860ebe4728b1fe5d3e55e5d81b
rubinaldod <rubinaldomingos@gmail.com>
2023-03-13 15:27:52 +0300

    header and home

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 
 const Header =()=>{
+
+    // Toggle menu
+    const [Toggle, showmenu] = useState(false);
     return(
-       <header>
+       <header className='header'>
         <nav className="nav container">
             <a href="index.html" className="nav_logo">Rubinaldo</a>
-            <div className="menu">
+            <div className={Toggle ? 'nav_menu show_menu' :  "nav_menu"}>
                 <ul className="nav_list grid">
-                    <li className="navitem"><a href="" className="nav_link"><i className="uil uil- nav_icon"></i></a></li>
+
+                    <li className="nav_item">
+                        <a href="#home" className="nav_link active_link">
+                        <i className="uil uil-estate nav_icon"></i>
+                        Home
+                        </a>
+                    </li>
+
+                    <li className="navi_tem"><a href="#about" className="nav_link">
+                        <i className="uil uil-user nav_icon"></i>
+                        About
+                        </a>
+                    </li>
+
+                    <li className="nav_item"><a href="#skills" className="nav_link">
+                        <i className="uil uil-file-alt nav_icon"></i>
+                       Skills
+                        </a>
+                    </li>
+
+                    <li className="nav_item"><a href="#services" className="nav_link">
+                        <i className="uil uil-briefcase nav_icon"></i>
+                       Services
+                        </a>
+                    </li>
+                    
+                    <li className="nav_item"><a href="#portifolio" className="nav_link">
+                        <i className="uil uil-scenery nav_icon"></i>
+                       Portifolio
+                        </a>
+                    </li>
+                    
+                    <li className="nav_item"><a href="#skills" className="nav_link">
+                        <i className="uil uil-message nav_icon"></i>
+                       Contact
+                        </a>
+                    </li>
                 </ul>
+
+                <i className="uil uil-times nav_close" onClick={() => showmenu(!Toggle)} ></i>
+            </div>
+            <div className="nav_toggle" onClick={() => showmenu(!Toggle) }>
+                <i className="uil uil-apps"></i>
             </div>
         </nav>
        </header>
---
+++ src/components/home/social.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Social =()=>{
+    return(
+     <div className="home_social">
+        
+        <a href="https://www.instagram.com/rubinaldomingos/" className="home_social_icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="instagram.com" className="home_social_icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+        </a>
+
+        <a href="instagram.com" className="home_social_icon" target="_blank">
+        <i className="uil uil-linkedin"></i>
+        </a>
+     </div>
+    )
+}
+
+export default Social
