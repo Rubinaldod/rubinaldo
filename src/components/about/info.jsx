056b15c2ad5f12f7eaf552b04ae062f6332972c9
rubinaldod <rubinaldomingos@gmail.com>
2023-03-20 16:29:17 +0300

    working on about

---
+++ src/components/about/info.jsx
@@ -0,0 +1,37 @@
+import React from "react"; 
+import './about.css';
+
+
+
+const Info =() =>{
+    return(
+      <div className="about_info grid">
+            <div className="about_box">
+                <i className="bx bx-award"></i>
+                <h3 className="about_title">Experience </h3>
+                    <span className="about_subtitle">
+                    5 years of work
+                    </span>
+                
+            </div>
+
+            <div className="about_box">
+                <h3 className="about_title"> Completed</h3>
+                    <span className="about_subtitle">
+                    20 + Projects
+                    </span>
+                
+            </div>
+
+            <div className="about_box">
+                <h3 className="about_title">Support </h3>
+                    <span className="about_subtitle">
+                        Online 24/7
+                    </span>
+                
+            </div>
+      </div>
+    )
+}
+
+export default Info;
