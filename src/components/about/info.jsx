cc4f72b274354711b34cd9e88f6a9ff31bacaae1
Rubinaldod <rubinaldomingos@gmail.com>
2023-03-22 00:08:14 +0200

    about done

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -7,7 +7,7 @@ const Info =() =>{
     return(
       <div className="about_info grid">
             <div className="about_box">
-                <i className="bx bx-award"></i>
+                <i className="bx bx-award about_icon"></i>
                 <h3 className="about_title">Experience </h3>
                     <span className="about_subtitle">
                     5 years of work
@@ -16,6 +16,8 @@ const Info =() =>{
             </div>
 
             <div className="about_box">
+
+                <i className="bx bx-briefcase-alt about_icon"></i>
                 <h3 className="about_title"> Completed</h3>
                     <span className="about_subtitle">
                     20 + Projects
@@ -24,6 +26,7 @@ const Info =() =>{
             </div>
 
             <div className="about_box">
+                <i className="bx bx-support about_icon"></i>
                 <h3 className="about_title">Support </h3>
                     <span className="about_subtitle">
                         Online 24/7
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills =() => {
+    return (
+<section className="section skills" id="skills">
+<h2 className="section_title">Skills</h2>
+<span className="section_subtitle">
+My technical level
+</span>
+</section>
+    );
+};
+
+export default Skills;
